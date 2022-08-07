7629a1b9db1cfa9c8f83159d109710042181a432
LumaC0 <finsp01@gmail.com>
2022-08-07 11:49:01 -0500

    adds enum op types

--- 07/translator.c
+++ 07/translator.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
 #include "translator.h"
 
@@ -8,16 +9,104 @@
 #include <inttypes.h>
 #endif
 
+/** TODO match command type */
+/** TODO determine arg1 and arg2 from command type */
+const char *command_type(char *op)
+{
+        const char *c_op;
+        return "nothing";
+}
+
+/*{{{split_string(char **str_lst, char *vm_nstr)*/
+int split_string(char **str_lst, char *vm_nstr)
+{
+        char *t_nstr = vm_nstr;
+        char *tmp_str;
+        char c;
+        int l_inc = 0, t_inc = 0;
+
+        while (1) {
+                c = *(t_nstr+t_inc);
+                if (c == 32) {
+                        tmp_str = (char*)malloc(sizeof(char)*16);
+                        strncpy(tmp_str, t_nstr, t_inc);
+                        *(str_lst+l_inc++) = tmp_str;
+                        t_nstr+=t_inc;
+                        t_nstr++;
+                        t_inc = 0;
+               }
+                if (c == 0) {
+                        tmp_str = (char*)malloc(sizeof(char)*16);
+                        strncpy(tmp_str, t_nstr, --t_inc);
+                        *(str_lst+l_inc) = tmp_str;
+                        break;
+                }
+                t_inc++;
+        }
+        return 0;
+}
+/*}}}*/
+
+/*{{{parse(char *vm_nstr, FILE *outfile)*/
+int parse(char *vm_nstr, FILE *outfile)
+{
+        int c;
+        if ((c = *vm_nstr) == '/' || c == '\0' || c == '\n' || c == '\r')
+                return 0;
+
+        char **str_lst = malloc(sizeof(uintptr_t)*3);
+        split_string(str_lst, vm_nstr);
+
+        const char *command;
+        command = command_type(*(str_lst++));
+        return 0;
+}
+/*}}}*/
+
+/*{{{main*/
+/** translates VM code into hack assembly code */
 int main(int argc, char *argv[])
 {
 	char *in, *out;
-	intptr_t ptr;
-	
-	while (--argc) {
-		if (strcmp(*(argv+argc), "-o") == 0)
-			out = *(argv+argc+1);
-		in = *(argv+argc);
-	}
-	printf("%s %s\n", in, out);
-	
+
+        if (argc == 1) {
+                printf("Must specify an input file name with .vm extention.\n");
+                return ERROR;
+        }
+        if (strstr(*(argv+1), ".vm" ) == NULL) {
+                printf("Must specify an input file name with .vm extention.\n");
+                return ERROR;
+        }
+
+	in = *(argv+1);
+        out = "out.asm";
+
+	while (--argc)
+		if (strcmp(*(argv+argc), "-o") == 0) {
+                        if (strstr(*(argv+argc+1), ".asm" ) != NULL) {
+                                out = *(argv+argc+1);
+                        } else {
+                                printf("output file must have .asm extention.\n");
+                                return ERROR;
+                        }
+                }
+
+        FILE *infile, *outfile;
+        char vm_nstr[MAX_LINE_LENGTH];
+
+        if ((infile = fopen(in, "r")) == NULL) {
+                printf("Error occured reading input file");
+                return ERROR;
+        }
+
+        if ((outfile = fopen(out, "w")) == NULL) {
+                printf("Error occured opening output file");
+                return ERROR;
+        }
+
+        while (fgets(vm_nstr, MAX_LINE_LENGTH, infile) != NULL)
+                parse(vm_nstr, outfile);
+
+        return 0;
 }
+/*}}}*/
--- 07/translator.h
+++ 07/translator.h
@@ -1,19 +1,56 @@
 #include <inttypes.h>
 
+typedef int16_t DATA;				/* Signed 16-bit int is the only hack datatype */
 
 struct m_segment {
-	char **memory;            /* Pointer to array of pointers representing value stack */
-	const int16_t addr;       /* address in RAM, i.e. 0 - 15 */
+	DATA **memory;            /* Pointer to array of pointers representing value stack */
+	// const int16_t addr;       [> address in RAM, i.e. 0 - 15 <]
 	const int16_t base;				/* Base address of segment and address of first value  */
-	const int16_t upper;      /* Upper limit of possible values in stack */
+	// const int16_t upper;      [> Upper limit of possible values in stack <]
 	int16_t ith;              /* Ith element of value in stack */
 };
 
-#define C_PUSH(data, segment) (segment->(memory+segment->ith)) = data
-#define C_POP(data, segment) (segment->(memory+segment->ith))
+struct m_segment SP, LCL, ARG, THIS, THAT;
 
-struct m_segment SP, LCL, ARG, THIS, THAT, TEMP5, TEMP6, TEMP7, TEMP8, TEMP9, TEMP10, TEMP11, TEMP12, R13, R14, R15;
+#define MAX_LINE_LENGTH 100		/* Max length of read for fgets */
+#define ERROR -1              /* Return value when error is encountered */
 
-const char *command_type(const char* arg);  /* returns constant representing the type of command */
-struct m_segment *addr_to_mem(int addr);           /* translates memory address to segment name */
+#define PUSH(data, segment) (segment->(memory+segment->ith)) = data
+#define POP(data, segment) (segment->(memory+segment->ith))
+
+/* #define C_ARITHMETIC "C_ARITHMETIC" // */
+/* #define C_PUSH "C_PUSH"						  // */
+/* #define C_POP "C_POP"							  // */
+/* #define C_LABEL "C_LABEL"					  // */
+/* #define C_GOTO "C_GOTO"						  // bytecode command options */
+/* #define C_IF "C_IF"								  // */
+/* #define C_FUNCTION "C_FUNCTION"		  // */
+/* #define C_RETURN "C_RETURN"				  // */
+/* #define C_CALL "C_CALL"						  // */
+
+enum OPS {
+	C_ARITHMETIC,
+	C_PUSH,
+	C_POP,
+};
+
+struct op_code {
+	char *vm_code;
+	enum OPS op;
+};
+struct op_code op_codes[] = {
+	"push", C_PUSH,
+	"pop", C_POP,
+	"add", C_ARITHMETIC,
+	"sub", C_ARITHMETIC
+};
+
+/* how many op codes */
+#define HM_OP_CODES (sizeof op_codes/sizeof(struct op_code))
+
+const char *operation(char *op);								/* finds type of current commnad */
+// const char command_type(const char* arg);		 [> returns constant representing the type of command <]
+// struct m_segment *addr_to_mem(int addr);			 [> translates memory address to segment name <]
+int parse(char *vm_nstr, FILE *outfile);				/* Entrypoint for instruction translation */
+int split_string(char **str_lst, char *vm_nstr);/* utility to split string into list of strings */
 
