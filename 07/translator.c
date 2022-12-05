f989e6386c96674b8c6417704f3e47906f7eb78c
Spencer Finkel <finsp01@gmail.com>
2022-12-05 04:49:28 -0500

    successfully writes asm of SimpleAdd program

--- 07/translator.c
+++ 07/translator.c
@@ -26,7 +26,8 @@ struct tok {
     char *buf;                  // full line read from file
     char *cur;                  // current token
     char *memseg;               // memory segment
-    int ind;                    // current index being read
+    char *ind;                  // index (third argument) of push/pop
+    int line_ind;               // index of character being read
     enum command_type *command; // type of command
     struct file *file;          // input file
 };
@@ -41,71 +42,108 @@ int free_tok(struct tok *, int);
 
 #define MAX_LINE 256
 
-#define is_filename_start_valid(c) (\
+#define IS_FILENAME_START_VALID(c) (\
             (c >= 'A' && c <= 'Z'))
 
-#define is_line_comment_or_empty(c) (\
+#define IS_LINE_COMMENT_OR_EMPTY(c) (\
             (c == '/' || c == '\n' || c == '\r'))
 
-#define is_character_alphanumeric(c) (\
+#define IS_CHARACTER_ALPHANUMERIC(c) (\
             (c >= '!' && c <= '~'))
 
 int fname_check(char *fname) {
-    if (!is_filename_start_valid(fname[0])) return -1;
+    if (!IS_FILENAME_START_VALID(fname[0])) return -1;
     if (strcmp(".vm", fname+strlen(fname)-3) != 0) return -1; 
     return 0;
 }
 
-char *determine_memseg(struct tok *tok) {
+void determine_memseg_index(struct tok *tok) {
     size_t buf_size = strlen(tok->buf)+1;
     char *word = calloc(buf_size + 1, sizeof(char));
     int i = 0;
 
     for (;;) {
-        if (!is_character_alphanumeric(tok->buf[tok->ind])) {
-            ++tok->ind;
+        if (!IS_CHARACTER_ALPHANUMERIC(tok->buf[tok->line_ind])) {
+            ++tok->line_ind;
             continue;
         }
         break;
     }
     for(;;) {
-        if (!is_character_alphanumeric(tok->buf[tok->ind])) {
+        if (!IS_CHARACTER_ALPHANUMERIC(tok->buf[tok->line_ind])) {
             break;
         }
-        word[i++] = tok->buf[tok->ind++];
+        word[i++] = tok->buf[tok->line_ind++];
+    }
+    free(tok->cur);
+    tok->cur = word;
+    tok->ind = word;
+}
+
+void determine_memseg(struct tok *tok) {
+    size_t buf_size = strlen(tok->buf)+1;
+    char *word = calloc(buf_size + 1, sizeof(char));
+    int i = 0;
+
+    for (;;) {
+        if (!IS_CHARACTER_ALPHANUMERIC(tok->buf[tok->line_ind])) {
+            ++tok->line_ind;
+            continue;
+        }
+        break;
+    }
+    for(;;) {
+        if (!IS_CHARACTER_ALPHANUMERIC(tok->buf[tok->line_ind])) {
+            break;
+        }
+        word[i++] = tok->buf[tok->line_ind++];
     }
     free(tok->cur);
     tok->cur = word;
 
-    if (strcmp("constant", tok->cur)) {
-        tok->memseg = NULL;
+    int c;
+    if (strcmp("constant", tok->cur) == 0) {
+        determine_memseg_index(tok);
+        char *memseg = calloc(5, sizeof(char));
+        strcpy(memseg, "@");
+        strcat(memseg, tok->ind);
+        tok->memseg = memseg;
     }
-    else if (strcmp("local", tok->cur)) {
+    else if (strcmp("local", tok->cur) == 0) {
         char *memseg = calloc(5, sizeof(char));
         strcpy(memseg, "@LCL");
         tok->memseg = memseg;
+        determine_memseg_index(tok);
+
     }
-    else if (strcmp("argument", tok->cur)) {
+    else if (strcmp("argument", tok->cur) == 0) {
         char *memseg = calloc(5, sizeof(char));
         strcpy(memseg, "@ARG");
         tok->memseg = memseg;
+        determine_memseg_index(tok);
     }
-    else if (strcmp("this", tok->cur)) {
+    else if (strcmp("this", tok->cur) == 0) {
         char *memseg = calloc(6, sizeof(char));
         strcpy(memseg, "@THIS");
         tok->memseg = memseg;
+        determine_memseg_index(tok);
     }
-    else if (strcmp("that", tok->cur)) {
+    else if (strcmp("that", tok->cur) == 0) {
         char *memseg = calloc(6, sizeof(char));
         strcpy(memseg, "@THAT");
         tok->memseg = memseg;
+        determine_memseg_index(tok);
     }
-    else if (strcmp("static", tok->cur)) {
+    else if (strcmp("static", tok->cur) == 0) {
         char *memseg = calloc(strlen(tok->file->bn)+2, sizeof(char));
         strcpy(memseg, strcat("@", tok->file->bn));
         tok->memseg = memseg;
+        determine_memseg_index(tok);
     }
-    printf("%s\n", tok->memseg);
+    else {
+        fprintf(stderr, "%s is not a valid memory segment\n", tok->cur);
+        free_tok(tok, CLOSE_FILE);
+    }
 }
 
 void determine_command_type(struct tok *tok) {
@@ -113,46 +151,70 @@ void determine_command_type(struct tok *tok) {
     char *word = calloc(buf_size + 1, sizeof(char));
     int i;
     for (i = 0; i <= (int) buf_size +1; i++) {
-        if (!is_character_alphanumeric(tok->buf[i])) {
-            tok->ind = i;
+        if (!IS_CHARACTER_ALPHANUMERIC(tok->buf[i])) {
+            tok->line_ind = i;
             break;
         }
-        tok->ind = i;
+        tok->line_ind = i;
         word[i] = tok->buf[i];
     }
     word[++i] = '\0';
     tok->cur = word;
 
     enum command_type t;
-    char *memseg;
     if (strcmp(tok->cur, "push") == 0) {
         t = C_PUSH;
-        memseg = determine_memseg(tok);
+        determine_memseg(tok);
     } 
     else if (strcmp(tok->cur, "pop") == 0) {
         t = C_POP;
-        memseg = determine_memseg(tok);
+        determine_memseg(tok);
     }
     else if (strcmp(tok->cur, "add") == 0) {
         t = C_ARITHMETIC;
-        memseg = NULL;
     }
     else if (strcmp(tok->cur, "sub") == 0) {
         t = C_ARITHMETIC;
-        memseg = NULL;
+    }
+    else if (strcmp(tok->cur, "neg") == 0) {
+        t = C_ARITHMETIC;
     }
     else {
         fprintf(stderr, "token \"%s\" not recognized\n", tok->cur);
         free_tok(tok, CLOSE_FILE);
     }
     tok->command = &t;
-    tok->memseg = memseg;
 }
 
-void code_writer(struct tok *tok) {
-    // if (tok->command == C_
-    printf("inside code writer\n");
+#define WRITE_ASM_ARITHMETIC(file, sign) (\
+            fprintf(file, "@SP\nM=M+1\nA=M\nD=M\n@SP\nM=M-1\nA=M\nM=M%cD\n@SP\nM=M+1\n", sign))
 
+#define WRITE_ASM_PUSH(file, seg, ind) (\
+            fprintf(file, "%s\nA=M+%s\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n", seg, ind)) 
+
+#define WRITE_ASM_POP(seg, ind) (\
+            fprintf(file,"@SP\nM=M-1\nA=M\nD=M\n%s\nA=M+%s\nM=D\n", seg, ind))
+
+void code_writer(struct tok *tok) {
+    FILE *ofp = tok->file->ofp;
+    switch(*tok->command) {
+        case C_ARITHMETIC:
+            char sign = !strcmp(tok->cur, "add") ? '+': '-';
+            WRITE_ASM_ARITHMETIC(ofp, sign);
+            break;
+
+        case C_PUSH:
+            WRITE_ASM_PUSH(ofp, tok->memseg, tok->ind);
+            break;
+
+        case C_POP:
+            WRITE_ASM_PUSH(ofp, tok->memseg, tok->ind);
+            break;
+
+        default:
+            fprintf(stderr, "%s\n", "Failed to write ASM to file");
+            free_tok(tok, CLOSE_FILE);
+    }
 }
 
 int read_lines(struct file *file) {
@@ -160,14 +222,12 @@ int read_lines(struct file *file) {
     struct tok *new_tok = malloc(sizeof *new_tok);
     new_tok->file = file;
     while (fgets(line, MAX_LINE, new_tok->file->fp) != NULL) {
-        if (is_line_comment_or_empty(line[0]))
+        if (IS_LINE_COMMENT_OR_EMPTY(line[0]))
             continue;
         new_tok->buf = (char *) &line;
         determine_command_type(new_tok);
         code_writer(new_tok);
     }
-
-    return 0;
 }
 
 struct file *open_file(char *fname) {
@@ -189,11 +249,12 @@ struct file *open_file(char *fname) {
     if ((fp = fopen(file->fn, "r")) == NULL) {
         fprintf(stderr, "%s does not exist\n", file->fn);
         close_file(file);
-
     }
+    file->fp = fp;
     strcpy(ofn, file->bn);
-    strcat(ofn, ".ast");
+    strcat(ofn, ".asm");
     file->ofn = ofn;
+    printf("%s\n", file->ofn);
     ofp = fopen(file->ofn, "w");
     file->ofp = ofp;
     return file;
